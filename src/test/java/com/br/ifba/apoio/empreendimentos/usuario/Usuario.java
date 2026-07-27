6550aded3724298c51cb097a5d296277a13b23e1
luis <20242ireads0019@ifba.edu.br>
2026-07-26 22:23:49 -0300

    Usuario teste estava dando erro no render

---
+++ src/test/java/com/br/ifba/apoio/empreendimentos/usuario/Usuario.java
@@ -0,0 +1,18 @@
+package com.br.ifba.apoio.empreendimentos.usuario;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Usuario {
+
+
+    private Long id;
+
+    private String nome;
+    private String email;
+    private String senha;
+}
