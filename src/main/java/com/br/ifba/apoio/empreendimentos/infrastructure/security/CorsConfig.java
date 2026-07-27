7a3f4def8eb23511149f0e080d9a6d9e86555ed2
luis <20242ireads0019@ifba.edu.br>
2026-07-27 10:56:23 -0300

    Adicionado suporte a CORS para o domínio da aplicação frontend

--- src/main/java/com/br/ifba/apoio/empreendimentos/infrastructure/security/CorsConfig.java
+++ src/main/java/com/br/ifba/apoio/empreendimentos/infrastructure/security/CorsConfig.java
@@ -20,13 +20,24 @@ public class CorsConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:3000"));
-        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
+
+        configuration.setAllowedOrigins(List.of(
+                "http://localhost:3000",
+                "https://frontend-prog04-web.vercel.app"
+        ));
+
+        configuration.setAllowedMethods(List.of(
+                "GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"
+        ));
+
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
 
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        UrlBasedCorsConfigurationSource source =
+                new UrlBasedCorsConfigurationSource();
+
         source.registerCorsConfiguration("/**", configuration);
+
         return source;
     }
 }
